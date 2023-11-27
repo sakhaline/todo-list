872d2c5bc46f5f0d6fbed04ae9b27bf7f80b28f5
sakhaline <alexandra.ivva@gmail.com>
2023-11-27 12:59:56 +0200

    Create tag list & task list URLs, views, templates

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from todolist import views
 
+urlpatterns = [
+    path("", views.TaskListView.as_view(), name="task-list"),
+    path("create/", views.TaskCreateView.as_view(), name="task-create"),
+    # path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
+    # path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
+
+    path("tags/", views.TagListView.as_view(), name="tag-list"),
+    # path("tags/create/", views.TagCreateView.as_view(), name="tag-create"),
+    # path("tags/<int:pk>/update/", views.TagUpdateView.as_view(), name="tag-update"),
+    # path("tags/<int:pk>/delete/", views.TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todolist"
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todolist.models import Task, Tag
+from todolist.forms import TaskForm
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    fields = "__all__"
+    queryset = Task.objects.prefetch_related("tags")
+    print(queryset)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    fields = "__all__"
+
+
+class TaskCreateView(generic.CreateView):
+    form_class = TaskForm
+    template_name = "todolist/task_form.html"
+    success_url = reverse_lazy("todolist:task-list")
